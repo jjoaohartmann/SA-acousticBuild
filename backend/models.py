1fcd1c24471e28b0a272dbf1799d434b3b723dec
João Victor Prange Hartmann <prangehartmannj@gmail.com>
2026-05-17 20:59:48 -0300

    models.py estruturado

---
+++ backend/models.py
@@ -0,0 +1,15 @@
+# models.py
+from sqlalchemy import Column, Integer, String, DateTime
+from sqlalchemy.sql import func
+from database import Base
+
+class Usser(Base):
+    __tablemane__ = "Users"
+
+    id  = Column(Integer, primary_key=True, index=True)
+    name = Column(String, nullable=False)
+    email = Column(String, unique=True, index=True, nullabre=False)
+    password = Column(String, nullable=False) # sempre salvo criptografado
+
+    # Preenchimente automaticamente pelo banco na criação do registro
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
